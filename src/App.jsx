91cc0e1677cc0b6210143772a0ed0fed94ab3ead
AmerDje <amer.dje10n20@gmail.com>
2025-04-14 09:05:05 +0200

    styling

--- src/App.jsx
+++ src/App.jsx
@@ -23,50 +23,33 @@ const App = () => {
     //when multiple must have a parent element which is now a fragment <> or regular div
     //when you're passing props, values that are expressions (like arrays, objects, or variables) should be wrapped in curly braces ({}). This is because the curly braces tell React that you're passing JavaScript expressions as props rather than static values (like strings).
     // in a nutshell props are always passed between {} excepts if they are a plain string
-    <>
+    <div className="card-container">
       <h1>Function Component</h1>
-      <Card
-        title="Star Wars"
-        year="1977"
-        actors={[
-          { name: "Mark Hamill", age: 42 },
-          { name: "Harrison Ford", age: 79 },
-        ]}
-      />
-      <Card
-        title="Avatar"
-        year="2009"
-        actors={[
-          { name: "Sam Worthington", age: 40 },
-          { name: "Zoe Saldana", age: 36 },
-          { name: "Sigourney Weaver", age: 65 },
-        ]}
-      />
-      <Card
-        title="Lion King"
-        year="1994"
-        actors={[
-          {
-            name: "Natalie Portman",
-            age: 30,
-          },
-        ]}
-      />
-    </>
+      <Card title="Star Wars" year="1977" />
+      <Card title="Avatar" year="2009" />
+      <Card title="Lion King" year="1994" />
+    </div>
   );
 };
 
 //props are passed in as an object to the component
-const Card = ({ title, year, actors }) => {
+const Card = ({ title, year }) => {
   return (
     // when using props they should be between {}
-    <>
+    <div
+      className="card"
+      // style={{
+      //   border: "1px solid #4b5362",
+      //   padding: "20px",
+      //   margin: "10px",
+      //   backgroundColor: "#31363f",
+      //   borderRadius: "10px",
+      //   minHeight: "100px",
+      // }}
+    >
       <h2>{title}</h2>
       <p>{year}</p>
-      {actors.map((actor) => (
-        <p>{actor.name}</p>
-      ))}
-    </>
+    </div>
   );
 };
 export default App;
